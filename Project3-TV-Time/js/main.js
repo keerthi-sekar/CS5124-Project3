015376655012f592d30a5f88a769570eced86569
Keerthi Sekar <sekarku@mail.uc.edu>
2023-04-10 21:48:45 -0400

    put project 2 wordcloud into project3

--- Project3-TV-Time/js/main.js
+++ Project3-TV-Time/js/main.js
@@ -2,6 +2,11 @@ let data, barchartA, barchartB, wordcloud;
 let season_options = ["All Seasons"];
 let episode_options = ["All Episodes"];
 let cast = [];
+let words = [];
+let count = 0;
+
+let unwanted_words = ['and', 'is', 'the', 'the ', 'for', 'to', 'on', 'had', 'that', 'of', 'not', 'or', 'in', 
+                      'an', 'no', 'it', 'by', '&', 'further', 'through', 'a', 'this', "it'd"];
 //season,episode,character,line
 d3.csv('data/script.csv')
 .then(_data => {
@@ -23,21 +28,39 @@ d3.csv('data/script.csv')
           'Character': d.character,
           'Line': d.line
         }
+
+        if(count < 500)
+        {
+          des = d.line.replace('"', '');
+          des = des.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"");
+          des = des.toLowerCase();
+          des = des.split(' ');
+          for(let i = 0; i < unwanted_words.length; i++)
+          {
+            des = des.filter(f => f !== unwanted_words[i]);
+          }
+          words = words.concat(des);
+          count++;
+        }
+
         cast.push(datapoint);
+
       }
     });
 
-    console.log(cast);
+    console.log('words', words);
+    console.log('cast1', cast);
 
     cast = cast.sort(function (a,b) {return d3.ascending(a.Season, b.Season);});
     cast = cast.sort(function (a,b) {return d3.ascending(a.Episode, b.Episode);});
     //cast = cast.sort(function (a,b) {return d3.ascending(a.scene, b.scene);});
 
     var character_rollup = d3.rollups(cast, v => v.length, d => d.Character);
-    var line_rollup = d3.rollups(data, v => v.length, d => d.character);
+    var line_rollup = d3.rollups(words, v => v.length, d => d);
     var episode_rollup = d3.rollups(cast, v => v.length, d => d.Episode);
     var season_rollup = d3.rollups(cast, v => v.length, d => d.Season);
-    console.log('cast', line_rollup);
+
+    console.log("rollup", line_rollup);
 
     season_rollup.forEach(v => {
       season_options.push("Season " + v[0]);
@@ -86,10 +109,10 @@ d3.csv('data/script.csv')
 		
 		barchartB.updateVis();
 
-    /* wordcloud = new WordCloud({
+    wordcloud = new WordCloud({
       parentElement: '#chart4'
     }, line_rollup)
-    wordcloud.initVis(); */
+    wordcloud.initVis();
  
   })
   .catch(error => console.error(error));
